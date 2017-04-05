66b63244f2adac3bc250d6b433a0d4fcdc290ca1
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-05 14:59:33 -0700

    load templates with real data

--- renderer.js
+++ renderer.js
@@ -1,11 +1,21 @@
 let fs = require('fs');
 
+function mergeValues(values, content){
+  //Cycle over the keys
+    for (let key in values) {
+    //Replace all {{key}} with the value from the values object;
+      content = content.replace("{{" + key + "}}",values[key]);
+      }
+  //return merged content
+  return content;
+
+}
 
 function view(templateName, values, response) {
   //read from template files
-    var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
       //Insert values into the content
-
+      fileContents = mergeValues(values, fileContents);
       //Write out to the response
       response.write(fileContents);
 
