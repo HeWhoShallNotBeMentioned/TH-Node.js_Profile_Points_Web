dcda9db7248ef07f8a822e277ca1588c1db46ded
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-04 18:29:07 -0700

    connected to API, printed data to screen, and implemented an error catch

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 // Handle HTTP route GET / and POST i.e. Home
   function homeRoute(request, response) {
     //if url === "/" && GET
@@ -21,13 +23,32 @@
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/plain');
       response.write("Header\n");
-      response.write(username + "\n");
-      response.end('Footer\n');
       //get json from Treehouse
-        //on "end"
-          //show profile
-        //on "error"
-          //show error
+      var studentProfile = new Profile(username);
+      //on "end"
+      studentProfile.on("end", function(profileJSON){
+        //show profile
+
+        //store the value that we need
+        var values = {
+          avatarULR: profileJSON.gravatar_url,
+          username: profileJSON.profile_name,
+          badges: profileJSON.badges.length,
+          javaScriptPoints: profileJSON.points.JavaScript
+        };
+        //simple response
+        response.write(values.username + " has " + values.badges + " badges" + "\n");
+        response.write(values.avatarULR + "\n");
+        response.write(values.javaScriptPoints + "\n");
+        response.end('Footer\n');
+      });
+      //on "error"
+      studentProfile.on("error", function(error){
+        //show error
+        response.write(error.message + "\n");
+        response.end('Footer\n');
+      });
+
     }
   }
 
